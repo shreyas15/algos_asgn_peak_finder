f4bcf877c94c3b17dc7104f6d78d748097fa4f1b
shreyas bhat <ssubra15@uncc.edu>
2016-10-02 01:07:36 -0400

    version 0.2

--- algos_asgn_peak_finder/algos_asgn_peak_finder.cpp
+++ algos_asgn_peak_finder/algos_asgn_peak_finder.cpp
@@ -8,12 +8,15 @@
 #include "string"
 #include "sstream"
 #include "vector"
+#include "deque"
 
 using namespace std;
 
-void findRowsColumns(string inFileName, int& inArray);
+int** fileToArray(string inFileName);
 
-int main(int argc, char* argv[]) 
+//int inArray[1000][1000];
+
+int main() 
 {
 	//intro
 	cout << "Welcome to Peak Finder!" << endl;
@@ -21,54 +24,82 @@ int main(int argc, char* argv[])
 	cout << " **Note: The file and path cannot contain spaces**" << endl;
 	cout << endl;
 
-	string inFileName = argv[1];
+	string pathNFileName;
+	cin >> pathNFileName;
 
 	//declaring input and output file streams
 	ifstream inFile;
 	ofstream outFile;
+	cout << endl;	
 
-	inFile.open(inFileName);
-	cout << endl;
+	int** inArray = fileToArray(pathNFileName);
 
-	int inArray[1000][1000] = { {0} };
-	
-	//Checking for input of more than 1 file. If so, return as error.
-	if (argc != 2)
-	{
-		cerr << "Use only one file as input." << endl;
-		return 1;
-	}
-
-	/////////////inFile.open(argv[1]); //opening file
-
-
-	if (inFile.is_open()) //proceed only if file open or valid
-	{
-		findRowsColumns(inFileName, inArray[1000][1000]);
-		
-		inFile.close();
-	}
-
-	else
-	{
-		cout << "Please choose a valid input file" << endl << endl;
-	}
+	//findRowsColumns(pathNFileName);
 
     return 0;
 }
 
 
-void findRowsColumns(string inFileName, int & inArray)
+int** fileToArray(string inFileName)
 {
 	ifstream inFile;
-	inFile.open(inFileName);
+	int row = 1000, col = 1000;
+	int** my2DArray = new int*[row];
+
+	// Finding row and column counts
+	inFile.open(inFileName, ios_base::in);
 	if (inFile.is_open())
 	{
+		//cout << "success!" << endl;
+		string eachLine;
+		vector <int> getRow; //this vector size will give the column size
+		vector <int> getCol; //this vector size will give the row size
+		string tempString;
+		char tempChar;
+		bool rowStart = false;
 
+		while (inFile.good())
+		{
+			while (getline(inFile, tempString))
+			{
+				istringstream ss(tempString);
+				while( ss >> tempChar)
+				{
+					if (((tempChar - '0') == 43)) //this means we found a '['
+					{
+						continue;
+					}
+					else if ((tempChar - '0') == 45) //this means we found a ']'
+					{
+						rowStart = false;
+					}
+					else if ((tempChar - '0') == -4) //this means we found a ','
+					{
+						continue;
+					}
+					else if ((tempChar - '0') == -16) //this means we found a SPACE
+					{
+						continue;
+					}
+					else
+					{
+						rowStart = true;
+						getRow.push_back(tempChar - '0');
+					}
+
+					/*cout << "This symbol as is " << tempChar << endl;
+					cout << "This symbol in decimal is " << tempChar - '0' << endl;
+					cout << endl; */
+				}
+			}
+			
+		}
+
+		inFile.close();
 	}
 	else
 	{
-
+		cout << "fail :( " << endl;
 	}
-	//return 0;
+	return my2DArray;
 }
