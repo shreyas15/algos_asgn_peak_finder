4fd857520c97d33ce8bc7b8f0282b2d68b35f0cc
shreyas bhat <ssubra15@uncc.edu>
2016-10-01 21:32:49 -0400

    version 0.1.2

--- algos_asgn_peak_finder/algos_asgn_peak_finder.cpp
+++ algos_asgn_peak_finder/algos_asgn_peak_finder.cpp
@@ -4,18 +4,34 @@
 #include "stdafx.h"
 #include "iostream"
 #include "fstream"
+#include "iomanip"
+#include "string"
+#include "sstream"
+#include "vector"
+
 using namespace std;
 
-int findRowsColumns(string inputFile);
+void findRowsColumns(string inFileName, int& inArray);
 
-int main(int argc, char* argv[]) //Input file from command-line. 
+int main(int argc, char* argv[]) 
 {
+	//intro
 	cout << "Welcome to Peak Finder!" << endl;
+	cout << "Enter the name of the file to be read for input and press ENTER" << endl;
+	cout << " **Note: The file and path cannot contain spaces**" << endl;
+	cout << endl;
+
+	string inFileName = argv[1];
 
 	//declaring input and output file streams
 	ifstream inFile;
 	ofstream outFile;
 
+	inFile.open(inFileName);
+	cout << endl;
+
+	int inArray[1000][1000] = { {0} };
+	
 	//Checking for input of more than 1 file. If so, return as error.
 	if (argc != 2)
 	{
@@ -23,12 +39,12 @@ int main(int argc, char* argv[]) //Input file from command-line.
 		return 1;
 	}
 
-	inFile.open(argv[1]); //opening file
+	/////////////inFile.open(argv[1]); //opening file
 
 
 	if (inFile.is_open()) //proceed only if file open or valid
 	{
-		//findRowsColumns();
+		findRowsColumns(inFileName, inArray[1000][1000]);
 		
 		inFile.close();
 	}
@@ -41,8 +57,18 @@ int main(int argc, char* argv[]) //Input file from command-line.
     return 0;
 }
 
-int findRowsColumns(string inputFile)
-{
 
-	return 0;
+void findRowsColumns(string inFileName, int & inArray)
+{
+	ifstream inFile;
+	inFile.open(inFileName);
+	if (inFile.is_open())
+	{
+
+	}
+	else
+	{
+
+	}
+	//return 0;
 }
